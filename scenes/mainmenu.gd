3aefa72c1627c5e4c15952154d8a9ee35da57e1b
Arshea <arsheabimal@gmail.com>
2025-01-25 13:42:55 +0000

    Main menu with placeholder assets

---
+++ scenes/mainmenu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
