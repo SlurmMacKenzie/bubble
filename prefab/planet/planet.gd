62606750e0ec0d1762d652ec5686e0afeb2da822
amy-phillips <amy@islovely.co.uk>
2025-01-25 20:33:14 +0000

    add health bar

--- prefab/planet/planet.gd
+++ prefab/planet/planet.gd
@@ -1,2 +1,26 @@
 extends StaticBody2D
 class_name Planet
+
+signal take_damage(damage)
+
+signal planet_death_were_all_doomed()
+
+const health_max:int = 100
+var health:int = health_max
+
+func _ready() -> void:
+	take_damage.connect(_take_damage)
+	var health_bar = get_node("HealthBar")
+	health_bar.update_health_bar.emit(health)
+
+
+func _take_damage(damage):
+	health -= damage
+	health = clamp(health, 0, health_max)
+	
+	var health_bar = get_node("HealthBar")
+	health_bar.update_health_bar.emit(health)
+	
+	if health <= 0:
+		planet_death_were_all_doomed.emit()
+	
