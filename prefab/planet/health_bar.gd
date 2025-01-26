842aa335adced569a47ae31763c48a8b1933fa51
amy-phillips <amy@islovely.co.uk>
2025-01-26 13:42:07 +0000

    add atmosphere health bar

--- prefab/planet/health_bar.gd
+++ prefab/planet/health_bar.gd
@@ -1,9 +1,8 @@
 extends ProgressBar
 
-signal update_health_bar(health)
-
 func _ready() -> void:
-	update_health_bar.connect(_update_health_bar)
+	var planet: Planet = get_parent()
+	planet.update_health_bar.connect(_update_health_bar)
 
 func _update_health_bar(health: int) -> void:
 	value = health
--- prefab/planet/planet.gd
+++ prefab/planet/planet.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 class_name Planet
 
 signal take_damage(damage)
+signal update_health_bar(health)
 
 signal planet_death_were_all_doomed()
 
@@ -12,16 +13,14 @@ func _ready() -> void:
 	position = get_viewport_rect().size / 2
 	
 	take_damage.connect(_take_damage)
-	var health_bar = get_node("HealthBar")
-	health_bar.update_health_bar.emit(health)
+	update_health_bar.emit(health)
 
 
 func _take_damage(damage):
 	health -= damage
 	health = clamp(health, 0, health_max)
 	
-	var health_bar = get_node("HealthBar")
-	health_bar.update_health_bar.emit(health)
+	update_health_bar.emit(health)
 	
 	if health <= 0:
 		planet_death_were_all_doomed.emit()
