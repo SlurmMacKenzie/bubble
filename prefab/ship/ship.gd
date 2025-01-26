c681a4e43c10a61991477038bf9367ca813714e4
SlurmMacKenzie <guillaume.chevrier@gmail.com>
2025-01-26 15:21:40 +0100

    9 bubble per day

--- prefab/ship/ship.gd
+++ prefab/ship/ship.gd
@@ -4,9 +4,9 @@ class_name Ship
 
 func update_sprite(remaining_bubble: int, anim:bool = false):
 	match remaining_bubble:
-		3: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_001.png")
-		2: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_002.png")
-		1: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_003.png")
+		7,8,9: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_001.png")
+		4,5,6: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_002.png")
+		1,2,3: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_003.png")
 		0: %ShipSprite.texture = load("res://assets/art/ship_sprite/ship_004.png")
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_ELASTIC)
